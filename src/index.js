c7cda73db7461e33563229a5606914d04d21cfa0
Colson <colsoncalifornication@gmail.com>
2023-07-27 18:08:56 +0545

    connect redux to react + resolve redux test error with Root component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
-
+import Root from 'Root';
 import App from 'components/App';
 
 const container = document.getElementById('root');
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Root>
     <App />
-  </Provider>,
+  </Root>,
   container
 );
