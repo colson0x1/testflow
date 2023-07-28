f8f2de2701d5805aed28433b5bbee3af0a5e3f8b
Colson <colsoncalifornication@gmail.com>
2023-07-28 13:35:38 +0545

    resolve jest throwing axios import error on running tests

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -3,10 +3,20 @@ import { mount } from 'enzyme';
 import Root from 'Root';
 import App from 'components/App';
 
+/*
+The entire purpose of this test is to confirm 
+that our application can reach out to the JSON API 
+and fetch a lists of comments by itself
+*/
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the *entire* App
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
 
   // find the 'fetchComments' button and click it
 
   // Expect to find a list of comments
-})
+});
