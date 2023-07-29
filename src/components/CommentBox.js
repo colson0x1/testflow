5bcf0bdac879bfe3b9da443a2681bc83e0c907cc
Colson <colsoncalifornication@gmail.com>
2023-07-29 11:58:54 +0545

    place reusable logic to HOC

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,22 +5,6 @@ import * as actions from 'actions';
 class CommentBox extends Component {
   state = { comment: '' };
 
-  // component just got rendered
-  componentDidMount() {
-    this.shouldNavigateAway();
-  }
-
-  // component just got updated (received props)
-  componentDidUpdate() {
-    this.shouldNavigateAway();
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      this.props.history.push('/');
-    }
-  }
-
   handleChange = (event) => {
     this.setState({ comment: event.target.value });
   };
@@ -51,10 +35,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    auth: state.auth,
-  };
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(CommentBox);
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,34 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function requireAuth(ChildComponent) {
   class ComposedComponent extends Component {
+    // component just got rendered
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    // component just got updated (received props)
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    }
+
     render() {
-      return <ChildComponent />
+      return <ChildComponent />;
     }
   }
 
-  return ComposedComponent;
+  function mapStateToProps(state) {
+    return {
+      auth: state.auth,
+    };
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 }
