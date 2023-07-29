179d268afaad52efb4a70fad746133f93ca5580a
Colson <colsoncalifornication@gmail.com>
2023-07-29 06:39:07 +0545

    add routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Root from 'Root';
 import App from 'components/App';
 
@@ -8,7 +8,9 @@ const container = document.getElementById('root');
 ReactDOM.render(
   <Root>
     <BrowserRouter>
-      <Route path="/" component={App} />
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </Root>,
   container
