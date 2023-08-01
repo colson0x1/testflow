cbbdfdbe005ea448efc26846ef0e79b3e637a570
Colson <colsoncalifornication@gmail.com>
2023-08-01 07:59:16 +0545

    forward actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,5 +1,13 @@
 const asyncMiddleware = ({ dispatch }) =>
   (next) =>
-  (action) => {};
+  (action) => {
+    // Check to see if the action 
+    // has a promise on its 'payload' property
+    // If it does, then wait for it to resolve
+    // If it doesn't, then send the action on to the next middleware
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+  };
 
 export default asyncMiddleware;
